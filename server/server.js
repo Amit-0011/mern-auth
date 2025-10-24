a59abb136897ea8f77116d3e0242c873d144f65c
Amit Kumar <kamit363693@gmail.com>
2025-10-24 20:58:55 +0530

    authController updated

--- server/server.js
+++ server/server.js
@@ -1,29 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import "dotenv/config";
-// import cookieParser from "cookie-parser";
-
-// import connectDB from "./config/mongodb.js";
-// import authRouter from './routes/authRoutes.js'
-// import userRouter from "./routes/userRoutes.js";
-
-// const app = express();
-// const port = process.env.PORT || 4000;
-// connectDB();
-
-// const allowedOrigins = ['http://localhost:5173']
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors({origin: allowedOrigins, credentials: true }));
-
-// // API Endpoints
-// app.get('/', (req, res)=> res.send("API Working"));
-// app.use('/api/auth', authRouter)
-// app.use('/api/user', userRouter)
-
-// app.listen(port, () => console.log(`Server started on PORT:${port} `));
-
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
@@ -37,11 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-// Updated allowed origins
-const allowedOrigins = [
-  'http://localhost:5173', // local development
-  'https://mern-auth-sv2h.vercel.app' 
-];
+const allowedOrigins = ['http://localhost:5173']
 
 app.use(express.json());
 app.use(cookieParser());
@@ -53,3 +23,4 @@ app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 
 app.listen(port, () => console.log(`Server started on PORT:${port} `));
+
